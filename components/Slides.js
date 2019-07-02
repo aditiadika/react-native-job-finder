92090d5912315c220f526a54d51ba9aeef753961
dika aditia <aditiaaadicka@gmail.com>
2019-07-02 13:24:54 +0700

    create welcome screen

--- components/Slides.js
+++ components/Slides.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, Dimensions } from "react-native";
+import { Button } from "react-native-elements";
+
+const screenWidth = Dimensions.get("window").width;
 
 class Slides extends Component {
+  renderLastSlides(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Onwards!"
+          raised
+          buttonStyle={styles.buttonStyle}
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(res => {
-      <View key={res.text} style={styles.slide}>
-        <Text style={styles.slideText}>{res.text}</Text>
-      </View>;
+    return this.props.data.map((res, index) => {
+      return (
+        <View
+          key={res.text}
+          style={[styles.slide, { backgroundColor: res.color }]}
+        >
+          <Text style={styles.slideText}>{res.text}</Text>
+          {this.renderLastSlides(index)}
+        </View>
+      );
     });
   }
+
   render() {
-      console.log(this.props.data)
     return (
-      <ScrollView horizontal style={{ flex: 1 }}>
+      <ScrollView horizontal style={{ flex: 1 }} pagingEnabled>
         {this.renderSlides()}
       </ScrollView>
     );
@@ -22,10 +44,15 @@ const styles = {
   slide: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    width: screenWidth
   },
   slideText: {
-    fontSize: 30
+    fontSize: 30,
+    color: "white"
+  },
+  buttonStyle: {
+    backgroundColor: "#0288d1"
   }
 };
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 
 import Slides from "../components/Slides";
 
 const SLIDE_DATA = [
   {
-    text: "Welcome To Jobs Street"
+    text: "Welcome To Jobs Street",
+    color: "#03A9F4"
   },
   {
-    text: "Use this to get a job"
+    text: "Use this to get a job",
+    color: "#009688"
   },
   {
-    text: "Set your location, then swipe away"
+    text: "Set your location, then swipe away",
+    color: "#03A9F4"
   }
 ];
 
 export default class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate("auth");
+  };
+
   render() {
-    return <Slides data={SLIDE_DATA} />;
+    return (
+      <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
+    );
   }
 }
